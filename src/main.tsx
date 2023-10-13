7d182533e20af0bf144af2e4a5fe3868e9d8e1d7
Pony Hsieh <lbjwin@gmail.com>
2023-10-13 15:15:25 +0800

    build: 新增 react-router-dom 及相關設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import About from './About.tsx'
 import './index.css'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+  ],
+  {
+    basename:
+      process.env.NODE_ENV === 'production' ? '/react-18-github-actions/' : '/',
+  },
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
